8d2e35a379465efee25c947a3db580fe793090d3
AdrieMota <AdrieMota@uniplaclages.edu.br>
2025-08-31 17:29:04 -0300

    Adição do meu nome ao program.

--- Back/Spotifei/Program.cs
+++ Back/Spotifei/Program.cs
@@ -2,7 +2,7 @@
 System.Console.WriteLine("Ricardo\n");
 System.Console.WriteLine("Marcos Eduardo Wolff Costa\n");
 System.Console.WriteLine("Alan de Oliveira\n");
-System.Console.WriteLine("Adriele Mota\n");
+System.Console.WriteLine("Adriele Silva Mota \n");
 System.Console.WriteLine("João Victor de Oliveira Melo");
 
 

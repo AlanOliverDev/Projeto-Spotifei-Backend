a0e12d325d5970d879e17a5eab7ba47007b33fb7
Alan <alanrespawnables06@gmail.com>
2025-08-31 17:39:34 -0300

    alterado nome Alan de Oliveira

--- Back/Spotifei/Program.cs
+++ Back/Spotifei/Program.cs
@@ -1,9 +1,9 @@
 ﻿System.Console.WriteLine("Equipe Spotifei:\n");
 System.Console.WriteLine("Ricardo\n");
 System.Console.WriteLine("Marcos Eduardo Wolff Costa\n");
-System.Console.WriteLine("Alan de Oliveira\n");
 System.Console.WriteLine("Adriele Silva Mota \n");
 System.Console.WriteLine("João Victor de Oliveira Melo");
+System.Console.WriteLine("Alan de Oliveira\n");
 
 
 

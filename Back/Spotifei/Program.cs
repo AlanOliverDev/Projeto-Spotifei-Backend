c53089563714baf974eb9dc0e974446825a7d755
jvictormelo <jvmelo0x@gmail.com>
2025-08-31 17:19:47 -0300

    Att do nome João Victor

--- Back/Spotifei/Program.cs
+++ Back/Spotifei/Program.cs
@@ -3,7 +3,7 @@ System.Console.WriteLine("Ricardo\n");
 System.Console.WriteLine("Marcos Eduardo Wolff Costa\n");
 System.Console.WriteLine("Alan de Oliveira\n");
 System.Console.WriteLine("Adriele Mota\n");
-System.Console.WriteLine("João");
+System.Console.WriteLine("João Victor de Oliveira Melo");
 
 
 

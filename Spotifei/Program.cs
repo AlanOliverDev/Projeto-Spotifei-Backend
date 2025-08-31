1544791e0377b132cf93aba3996a261377bb1fcc
Alan <alanrespawnables06@gmail.com>
2025-08-31 15:20:42 -0300

    criado projeto .Net Spotifei e adicionado o nome dos integrantes do grupo

---
+++ Spotifei/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Equipe Spotifei:\n");
+System.Console.WriteLine("Ricardo\n");
+System.Console.WriteLine("Marcos Eduardo Wolff Costa\n");
+System.Console.WriteLine("Alan de Oliveira\n");
+System.Console.WriteLine("Adriele Mota\n");
+System.Console.WriteLine("João");
+
+
+
